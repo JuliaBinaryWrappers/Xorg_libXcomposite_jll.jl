f558059785c9805504b27172239a884c3ce38caa
jlbuild <juliabuildbot@gmail.com>
2026-02-14 19:41:30 +0000

    Xorg_libXcomposite_jll build 0.4.7+0

--- src/Xorg_libXcomposite_jll.jl
+++ src/Xorg_libXcomposite_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("Xorg_libXcomposite")
-JLLWrappers.@generate_main_file("Xorg_libXcomposite", UUID("3c9796d7-64a0-5134-86ad-79f8eb684845"))
+JLLWrappers.@generate_main_file("Xorg_libXcomposite", Base.UUID("3c9796d7-64a0-5134-86ad-79f8eb684845"))
 end  # module Xorg_libXcomposite_jll
